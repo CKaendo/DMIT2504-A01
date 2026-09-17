fe6406c69f904408663e4a47376f6474d7e09cdd
Oliver Antoniu <oliver+github@abolish.golf>
2026-09-17 11:04:59 -0600

    add [01 - intro]: ex04 - Step 4: calling API functions & rendering output

--- 01 - dart + flutter intro/ex04 - async + web data exercise/instructor/dictionary_api/bin/dictionary_api.dart
+++ 01 - dart + flutter intro/ex04 - async + web data exercise/instructor/dictionary_api/bin/dictionary_api.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 
 import 'package:dictionary_api/dictionary_api.dart' as dictionary_api;
 
-void main() {
+void main() async {
 
   // GAME PLAN:
   //  1. collect user input from terminal
@@ -12,8 +12,13 @@ void main() {
   /*
     2. pass that value on to form the complete API URL
     3. fetch the JSON (will make a generic helper function for url-string -> decoded JSON)
-    -> (optional) store the data in a class with late-initialised fields & a constructor
-    4. render the first definition out to terminal 
   */
+  final definition = await dictionary_api.getWordDefinition(word);
 
+  // -> (optional) store the data in a class with late-initialised fields & a constructor
+    
+  // 4. render the first definition out to terminal 
+  print('\nWord            Definition');
+  print('--------------  ---------------------------------------------------');
+  print('${word}      ${definition}');
 } 
