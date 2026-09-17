772d1345e1bcad58dedc9bd6dc25c101b98980b8
Oliver Antoniu <oliver+github@abolish.golf>
2026-09-17 10:54:55 -0600

    add [01 - intro]: ex04 - steps 2-3: generic function for fetching JSON

--- 01 - dart + flutter intro/ex04 - async + web data exercise/instructor/dictionary_api/lib/dictionary_api.dart
+++ 01 - dart + flutter intro/ex04 - async + web data exercise/instructor/dictionary_api/lib/dictionary_api.dart
@@ -1,3 +1,10 @@
-int calculate() {
-  return 6 * 7;
+import 'package:http/http.dart' as http; // import {pkgPath} as {name} lets you call things explicitly!
+                                         // e.g. http.get() rather than just get() 
+
+// - We want all our HTTP interaction to be async, so we have to return a Future<T>
+// - valid JSON can be of a variety of top-level types (e.g. {}, []) so our return type is "dynamic" (dart's version of Any)
+Future<dynamic> getJSON(String urlString) async {
+  final Uri url  = Uri.parse(urlString);  // parse the URL into a safe/confirmed type
+  final response = await http.get(url);   // http package fires only async HTTP requests, so we await
+  return jsonDecode(response.body);
 }
