7ee2ac7d257a8a949872ff29eaf4e0a229a742c9
Oliver Antoniu <oliver+github@abolish.golf>
2026-09-17 10:45:08 -0600

    add [01 - intro]: ex04 - dart starter app

---
+++ 01 - dart + flutter intro/ex04 - async + web data exercise/instructor/dictionary_api/bin/dictionary_api.dart
@@ -0,0 +1,12 @@
+import 'package:dictionary_api/dictionary_api.dart' as dictionary_api;
+
+void main() {
+
+  /* GAME PLAN:
+    1. collect user input from terminal
+    2. pass that value on to form the complete API URL
+    3. fetch the JSON (will make a generic helper function for url-string -> decoded JSON)
+    -> (optional) store the data in a class with late-initialised fields & a constructor
+    4. render the first definition out to terminal 
+  */
+}
